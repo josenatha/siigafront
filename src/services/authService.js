1f91f113979e3f056e102f8ddff1013140306d4d
josenatha <71301252+josenatha@users.noreply.github.com>
2024-05-23 08:34:09 -0700

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const logoutUser = async () => {
   try {
-    const response = await axios.post('http://siiga_backend.test/api/v1/logout');
+    const response = await axios.post('https://bkend-production.up.railway.app/api/v1/logout');
     return response.data;
   } catch (error) {
     throw error;
@@ -11,7 +11,7 @@ export const logoutUser = async () => {
 
 export const getUserByEmailAndPassword = async (email, password) => {
   try {
-    const response = await axios.post('http://siiga_backend.test/api/v1/login', {
+    const response = await axios.post('https://bkend-production.up.railway.app/api/v1/login', {
       email: email,
       password: password
     });
